289ca7d6dcbba144effb93bd21f40ba51c2ecc73
sjpark1120 <asgol1120@naver.com>
2025-11-24 23:50:49 +0900

    refactor: 메인페이지 수정

--- components/main.tsx
+++ components/main.tsx
@@ -1,23 +1,26 @@
-import styles from "../styles/main.module.css";
-export default function Main() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.circle}>
-        <span className={styles.topText}>PROTPOLIO</span>
-        <div className={styles.titleText}>FRONT-END</div>
-        <div className={styles.introText}>
-          사용자 경험을 위해 끊임없이 고민하고 노력합니다.
-          <br />
-          웹의 무한한 가능성을 탐구하는 프론트엔드 개발자 박수진입니다.
+import styles from "../styles/main.module.css"
+
+const Main = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.circle}>
+                <span className={styles.topText}>PORTFOLIO</span>
+                <div className={styles.titleText}>FRONT-END</div>
+                <div className={styles.introText}>
+                    사용자 경험을 위해 끊임없이 고민하고 노력합니다.
+                    <br />
+                    웹의 무한한 가능성을 탐구하는 프론트엔드 개발자 박수진입니다.
+                </div>
+            </div>
+            <div>
+                <div className={styles.miniText}>THIS IS MY PORTFOLIO</div>
+                <div className={styles.bottom}>
+                    <span className={styles.frontendText}>/FRONT END DEVELOPER</span>
+                    <span className={styles.frontendText}>2024</span>
+                </div>
+            </div>
         </div>
-      </div>
-      <div>
-        <div className={styles.miniText}>THIS IS MY PORTPOLIO</div>
-        <div className={styles.bottom}>
-          <span className={styles.frontendText}>/FRONT END DEVELOPER</span>
-          <span className={styles.frontendText}>2024</span>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
+
+export default Main
